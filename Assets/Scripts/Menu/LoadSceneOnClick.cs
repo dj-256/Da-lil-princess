190ce86316509ded3b95785d12b86f94b2039141
Mathieu-Schalkwijk <90777977+Mathieu-Schalkwijk@users.noreply.github.com>
2023-03-11 16:01:34 +0100

    Create game menu (#9)
    Press esc to open.

---
+++ Assets/Scripts/Menu/LoadSceneOnClick.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public string sceneName; // Le nom de la scène à charger
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
