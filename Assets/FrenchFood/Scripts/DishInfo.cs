bb4e7ac87c0230cef18b40392b9e4e216cc04b67
Mathieu Schalkwijk <mathieu.schalkwijk@gmail.com>
2023-04-07 21:37:42 +0200

    Complete every region dish

--- Assets/FrenchFood/Scripts/DishInfo.cs
+++ Assets/FrenchFood/Scripts/DishInfo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DishInfo : MonoBehaviour
 {
+    public string dishTitle = "Dish Title";
     public string dishInformation = "Information about the dish";
     private bool playerInside = false;
     private bool showInformation = false;
@@ -41,7 +42,7 @@ public class DishInfo : MonoBehaviour
 
     private void OnGUI()
     {
-        int fontSize = 24;
+        int fontSize = 22;
         GUIStyle style = new GUIStyle(GUI.skin.label);
         style.fontSize = fontSize;
         style.alignment = TextAnchor.UpperCenter;
@@ -59,11 +60,32 @@ public class DishInfo : MonoBehaviour
         if (showInformation)
         {
             DrawBlackBackground();
-            
+
+            GUIStyle titleStyle = new GUIStyle(GUI.skin.label);
+            titleStyle.fontSize = fontSize * 2;
+            titleStyle.alignment = TextAnchor.MiddleCenter;
+            titleStyle.normal.textColor = Color.white;
+
+            yPos = 50; // Move the title closer to the top
+            GUI.Label(new Rect(xPos, yPos, labelWidth, fontSize * 2 + 10), dishTitle, titleStyle);
+
             style.normal.textColor = Color.white;
-            style.alignment = TextAnchor.MiddleCenter;
-            yPos = (Screen.height - labelHeight) / 2;
-            GUI.Label(new Rect(xPos, yPos, labelWidth, labelHeight), dishInformation, style);
+            style.alignment = TextAnchor.UpperLeft;
+            style.wordWrap = true;
+
+            yPos += fontSize * 2 + 30; // Adjust the yPos for the description box
+
+            float boxWidth = labelWidth;
+            float boxHeight = Screen.height - yPos - 50;
+            Rect descriptionRect = new Rect(xPos, yPos, boxWidth, boxHeight);
+
+            GUIStyle boxStyle = new GUIStyle(GUI.skin.box);
+            boxStyle.alignment = TextAnchor.UpperLeft;
+            boxStyle.fontSize = fontSize;
+            boxStyle.normal.textColor = Color.white;
+            boxStyle.wordWrap = true;
+
+            GUI.Box(descriptionRect, dishInformation, boxStyle);
         }
     }
 }
